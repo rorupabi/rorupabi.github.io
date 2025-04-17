a8329df5ce8717fd4a6da464c6dff9a04860c447
Ro Padilla <157687882+rorupabi@users.noreply.github.com>
2025-04-17 15:13:35 +0800

    pr

---
+++ front/src/components/bodySection.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const bodySection = () => {
+  return (
+    <div>bodySection</div>
+  )
+}
+
+export default bodySection
--- front/src/components/navbar.jsx
+++ front/src/components/navbar.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
-
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <>
+      <header className="sticky top-0 z-[20] mx-auto flex w-full items-center justify-between p-8 bg-gradient-to-r from-gray-900 to-gray-700">
+        <h1> header</h1>
+        <div></div>
+      </header>
+    </>
   )
 }
 

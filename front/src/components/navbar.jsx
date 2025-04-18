76061c824b8b6e57e37948f0996ddd4c62fe279a
Ro Padilla <157687882+rorupabi@users.noreply.github.com>
2025-04-18 12:41:13 +0800

    navbar changes

--- front/src/components/navbar.jsx
+++ front/src/components/navbar.jsx
@@ -1,9 +1,10 @@
 const Navbar = () => {
   return (
     <>
-      <header className="sticky top-0 z-[20] mx-auto flex w-full items-center justify-between p-8 bg-gradient-to-r from-gray-900 to-gray-700">
-        <h1> header</h1>
-        <div></div>
+      <header className="overflow-x-hidden text-stone-300 antialiased">
+        <div className="fixed inset-0 -z-10">
+          <div class="relative h-full w-full bg-black"><div class="absolute bottom-0 left-0 right-0 top-0 bg-[linear-gradient(to_right,#4f4f4f2e_1px,transparent_1px),linear-gradient(to_bottom,#8080800a_1px,transparent_1px)] bg-[size:14px_24px]"></div><div class="absolute left-0 right-0 top-[-10%] h-[1000px] w-[1000px] rounded-full bg-[radial-gradient(circle_400px_at_50%_300px,#fbfbfb36,#000)]"></div></div>
+        </div>
       </header>
     </>
   )

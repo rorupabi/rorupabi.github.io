65dcd231325f3e91834613df77b81a8f64e8db5d
Ro Padilla <157687882+rorupabi@users.noreply.github.com>
2025-04-21 11:36:39 +0800

    Started with projects section

---
+++ front/src/components/projects.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const projects = () => {
+  return (
+    <div className='pb-4'>
+      <h2 className='my-20 text-center text-4xl'>Projects</h2>
+    </div>
+  )
+}
+
+export default projects

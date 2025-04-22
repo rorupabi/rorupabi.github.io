985d6657f0b878f2c4c26689e34c0ed71f45a2c9
Ro Padilla <157687882+rorupabi@users.noreply.github.com>
2025-04-22 20:53:09 +0800

    added experience, project should have images of project before making it

--- front/src/components/projects.jsx
+++ front/src/components/projects.jsx
@@ -4,6 +4,9 @@ const projects = () => {
   return (
     <div className='pb-4'>
       <h2 className='my-20 text-center text-4xl'>Projects</h2>
+      <div>
+        
+      </div>
     </div>
   )
 }

4af863517fef705f2b926982e24ac04450f72b33
Ro Padilla <157687882+rorupabi@users.noreply.github.com>
2025-04-25 20:52:45 +0800

    pr

--- front/src/constants/experiences.jsx
+++ front/src/constants/experiences.jsx
@@ -56,7 +56,7 @@ export const PROJECTS = [
     image: project3,
     description:
       "A personal portfolio website showcasing projects, skills, and contact information.",
-    technologies: ["HTML", "CSS", "React", "Bootstrap"],
+    technologies: ["HTML", "CSS", "ReactJS", "TailwindCSS", "NodeJS"],
   },
   {
     title: "24VR Page",
